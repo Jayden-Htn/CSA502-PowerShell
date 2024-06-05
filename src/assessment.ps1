abecb258fa1da811f22d94a81895471fa7391d36
Jayden H <jayden-houghton@live.nmit.ac.nz>
2024-06-05 16:11:43 +1200

    Finish off assessment script

--- src/assessment.ps1
+++ src/assessment.ps1
@@ -1,44 +1,69 @@
-# Display menu and get user choice
-Write-Host "Choose an option:"
-Write-Host "1. Display network configuration"
-Write-Host "2. List virtual machines"
-Write-Host "3. Test connection with a machine"
-$choice = Read-Host "Enter your choice (1, 2 or 3)"
+# Host and Virtual Machine testing Script
+# Jayden Houghton
+ 
+# Note: the following command may need to be run and accepted to allow 
+# scripts to be run on a new machine
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+ 
+ 
+# <========= Main Program =========>
+ 
+$stop = $false
+ 
+do {
+    # Display menu and get user choice
+    Write-Host ""
+    Write-Host "Choose option:"
+    Write-Host "1. Display network configuration"
+    Write-Host "2. List virtual machines"
+    Write-Host "3. Test connectivity with a machine"
+    Write-Host "4. End program"
+    Write-Host ""
+    $choice = Read-Host "Enter your choice (1, 2, 3 or 4)"
+    Write-Host ""
+ 
+    # Perform action based on user choice
+    switch ($choice) { 
+        1 { 
+            # Display network configuration 
+            Get-NetIPConfiguration | Out-String 
+            # Out string waits for string output to continue
+            $wait = Read-Host "Press enter to continue..." 
+        } 
+ 
+        2 { 
+            # List the virtual machines in the private subnet 
+            Get-VM | Out-String 
+            # Get-VM has no parameters so uses local host
+            # Out string waits for string output to continue
+            $wait = Read-Host "Press enter to continue..."
+        } 
 
-# Perform action based on user choice
-switch ($choice) {
-    1 {
-        # Display network configuration
-        Get-NetIPConfiguration
-        # https://www.pdq.com/blog/what-is-the-powershell-equivalent-of-ipconfig/
-    }
-    2 {
-        # List the virtual machines in the private subnet
-
-        
-
-    }
-    3 {
-        # Test connection with a machine
-        Write-Host ""
-        Write-Host "Enter a ip address to ping or 'exit' to return to the main menu"
-        $loop = $true
-        while ($loop) {
-            $choice = Read-Host "Enter the ip address"
-            switch ($choice) {
-                "exit" {
-                    $loop = $false
+        3 {
+            # Test connection with a machine 
+            Write-Host "Enter a IP address to ping or 'exit' to return to the main menu"  
+            $choice = Read-Host "IP address" 
+            switch ($choice) { 
+                "exit" { 
+                } 
+                default { 
+                    Write-Host ""
+                    Write-Host "Please wait, this may take some time."
+                    Test-Connection $choice 
+                    Write-Host ""
+                    $wait = Read-Host "Press enter to continue..."
                 }
-                default {
-                    Test-Connection $choice
-                    $loop = $false
-                }
-            }
+            } 
         }
-
-        # https://www.educba.com/powershell-ping/#:~:text=The%20PowerShell%20ping%20command%20is,and%20also%20by%20using%20the
+ 
+        4 {
+            # Exit program
+            $stop = $true
+        }
     }
-    default {
-        Write-Host "Invalid choice. Please enter 1, 2 or 3."
+         
+    default { 
+        # Handle invalid inputs
+        Write-Host "Invalid choice. Please enter 1, 2 or 3." 
     }
-}
+} until ($stop)
